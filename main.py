aefe86d9951cf2060c911a355a99b5a399ea4838
rivermont <william.11bennett@gmail.com>
2022-04-21 17:37:02 -0400

    Mostly finished data processing, moved inputs to data/

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 import csv
 import json
 
+
 overwrite = False  # for overwriting data files
 
 if overwrite:
@@ -13,17 +14,16 @@ if overwrite:
 
     # check whether files exist
     # save files
-    with open("./hpai-commercial-backyard-flocks.csv", "w+") as f:
+    with open("./data/hpai-commercial-backyard-flocks.csv", "w+") as f:
         f.write(commercial)
 
-    with open("hpai-wild-birds.csv", "w+") as f:
+    with open("./data/hpai-wild-birds.csv", "w+") as f:
         f.write(wild)
 
 
-# ['State', 'County', 'Date Confirmed', 'Flock Type', 'Flock Size']
 data = {}
 
-with open("./hpai-commercial-backyard-flocks.csv", "r") as f:
+with open("./data/hpai-commercial-backyard-flocks.csv", "r") as f:
     creader = csv.reader(f, delimiter=',', quotechar='"')
     header = next(creader)
     
@@ -39,11 +39,32 @@ with open("./hpai-commercial-backyard-flocks.csv", "r") as f:
         if county not in data[state]['counties']:
             data[state]['counties'][county] = {'description': '', 'num_birds_cap': 0, 'num_report_cap': 0}
         
-        data[state]['counties'][county]['description'] += ftype + ': ' + fsize + '\n'
+        data[state]['counties'][county]['description'] += ftype + ': ' + fsize + '<br>'
         data[state]['counties'][county]['num_birds_cap'] += int(fsize.replace(',', ''))
         data[state]['counties'][county]['num_report_cap'] += 1
         data[state]['num_birds_cap'] += int(fsize.replace(',', ''))  # can also automate this at the end
         data[state]['num_report_cap'] += 1
+
+
+
+
+
+def clean(geo, btype, data):
+    '''Strips geojson input of unneeded properties and adds new properties looked up in data.'''
+    out = {"type": "Feature", "geometry": geo["geometry"], "name": geo["properties"]["NAMELSAD"], "properties": {
+           "STATE_NAME": geo["properties"]["STATE_NAME"],
+           "NAME": geo["properties"]["NAME"]
+    }}
+    
+    out['properties']['description'] = data[out['properties']['STATE_NAME']]['counties'][out['properties']['NAME']]['description']
+    out['properties']['num_report_' + btype] = data[out['properties']['STATE_NAME']]['counties'][out['properties']['NAME']]['num_report_' + btype]
+    if btype == 'cap':
+        out['properties']['num_birds_' + btype] = data[out['properties']['STATE_NAME']]['counties'][out['properties']['NAME']]['num_birds_' + btype]
+    
+    return out
+
+
+###
 
 
 # create set of (state, county) pairs
@@ -53,24 +74,20 @@ for s in data:
         x.add((s, c))
 
 
-out = {"type": "FeatureCollection", "name": "HPAI Counties", "features": [],
-       "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::4269" } },}  # geojson to export
+out = {"type": "FeatureCollection", "name": "HPAI Counties CAPTIVE", "features": [],
+       "crs": {"type": "name", "properties": {"name": "urn:ogc:def:crs:EPSG::4269"}}
+       }  # geojson to export
 
-with open("./cb_2020_us_county_20m.geojson", 'r') as f:
+with open("./data/cb_2020_us_county_20m.geojson", 'r') as f:
     counties = json.loads(f.read())
     for county in counties['features']:
         if (county['properties']['STATE_NAME'], county['properties']['NAME']) in x:
-            # print(county['properties']['NAME'])
-            out['features'].append(county)
-            # TODO: add description and totals to output
+            x.remove((county['properties']['STATE_NAME'], county['properties']['NAME']))
+            out['features'].append(clean(county, 'cap', data))
+    print(x)  # TODO: fix these counties not being found
 
-with open("./hpai_counties.geojson", "a", encoding="utf-8") as f:
+with open("./hpai_counties_cap.geojson", "a", encoding="utf-8") as f:
     json.dump(out, f)
-
-# parse csvs, reading into dict:
-# {state: {counties: {county1: {...}, ...}, num_birds_wild: 0, num_report_wild: 0, num_birds_cap: 0, num_report_cap: 0}, ...}
-
-# For state in geojson, if state in list, add to file
 
 
 ############
@@ -79,7 +96,7 @@ with open("./hpai_counties.geojson", "a", encoding="utf-8") as f:
 
 data = {}
 
-with open("./hpai-wild-birds.csv", "r") as f:
+with open("./data/hpai-wild-birds.csv", "r") as f:
     creader = csv.reader(f, delimiter=',', quotechar='"')
     header = next(creader)
     
@@ -95,7 +112,7 @@ with open("./hpai-wild-birds.csv", "r") as f:
         if county not in data[state]['counties']:
             data[state]['counties'][county] = {'description': '', 'num_report_wild': 0}
         
-        data[state]['counties'][county]['description'] += date + ': ' + ftype + '\n'
+        data[state]['counties'][county]['description'] += date + ': ' + ftype + '<br>'
         data[state]['counties'][county]['num_report_wild'] += 1
         data[state]['num_report_wild'] += 1
 
@@ -108,16 +125,19 @@ for s in data:
 
 
 out = {"type": "FeatureCollection", "name": "HPAI Counties WILD", "features": [],
-       "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:EPSG::4269" } },}  # geojson to export
+       "crs": {"type": "name", "properties": {"name": "urn:ogc:def:crs:EPSG::4269"}}
+       }  # geojson to export
 
-with open("./cb_2020_us_county_20m.geojson", 'r') as f:
+with open("./data/cb_2020_us_county_20m.geojson", 'r') as f:
     counties = json.loads(f.read())
     for county in counties['features']:
         if (county['properties']['STATE_NAME'], county['properties']['NAME']) in x:
-            # print(county['properties']['NAME'])
-            out['features'].append(county)
-            # TODO: add description and totals to output
+            x.remove((county['properties']['STATE_NAME'], county['properties']['NAME']))
+            out['features'].append(clean(county, 'wild', data))
+    print(x)  # TODO: fix these counties not being found
 
-with open("./hpai_counties-w.geojson", "a", encoding="utf-8") as f:
+with open("./hpai_counties_wild.geojson", "a", encoding="utf-8") as f:
     json.dump(out, f)
+
+# For state in geojson, if state in list, add to file
 
